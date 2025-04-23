92bf7e5c911218b62719f7ddd3bf8bbdc5b2430e
Ondřej Fíla <ondrej.fila@student.spsmb.cz>
2025-04-23 10:29:48 +0200

    part1

---
+++ TestGame/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestGame;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D playerTexture;
+    private Vector2 playerPosition;
+    
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        playerTexture=Content.Load<Texture2D>("spritesheet");
+        playerPosition =new Vector2(200,200);
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        
+        _spriteBatch.Begin();
+        
+        Rectangle rectangle = new Rectangle(0, 0, 8, 8);
+        _spriteBatch.Draw(playerTexture, playerPosition, rectangle, Color.White);
+        
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
