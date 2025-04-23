45716202961da7d9f5ee6586aa90fc1ee525f7ae
Ondřej Fíla <ondrej.fila@student.spsmb.cz>
2025-04-23 11:30:47 +0200

    part7

--- TestGame/Game1.cs
+++ TestGame/Game1.cs
@@ -59,60 +59,120 @@ public class Game1 : Game
     }
     
     protected override void Update(GameTime gameTime)
+{
+    if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        Exit();
+
+    KeyboardState keyboardState = Keyboard.GetState();
+
+    // --- Vodorovný pohyb (A, D)
+    float moveSpeed = 3f;
+    if (keyboardState.IsKeyDown(Keys.A))
+        playerPosition.X -= moveSpeed;
+    if (keyboardState.IsKeyDown(Keys.D))
+        playerPosition.X += moveSpeed;
+
+    // --- Skákání (W, SPACE)
+    float jumpStrength = -10f;
+    if (isOnGround && (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Space)))
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
-        KeyboardState keyboardState = Keyboard.GetState();
-
-        // --- Vodorovný pohyb (A, D)
-        float moveSpeed = 3f;
-        if (keyboardState.IsKeyDown(Keys.A))
-            playerPosition.X -= moveSpeed;
-        if (keyboardState.IsKeyDown(Keys.D))
-            playerPosition.X += moveSpeed;
-
-        // --- Skákání (W, SPACE)
-        float jumpStrength = -10f;
-        if (isOnGround && (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Space)))
-        {
-            playerVelocity.Y = jumpStrength;
-            isOnGround = false;
-        }
-
-        // --- Gravitace
-        playerVelocity.Y += gravity;
-
-        // --- Aktualizace pozice hráče
-        playerPosition += new Vector2(0, playerVelocity.Y);
-
-        // --- Kontrola spodního okraje okna (dopad)
-        if (playerPosition.Y + playerTexture.Height >= windowHeight)
-        {
-            playerPosition.Y = windowHeight - playerTexture.Height;
-            playerVelocity.Y = 0;
-            isOnGround = true;
-        }
-        else
-        {
-            isOnGround = false;
-        }
-
-        // --- Kontrola horního okraje okna
-        if (playerPosition.Y < 0)
-        {
-            playerPosition.Y = 0;
-            playerVelocity.Y = 0;
-        }
-
-        // --- Omezení pohybu vlevo a vpravo
-        if (playerPosition.X < 0)
-            playerPosition.X = 0;
-        if (playerPosition.X + playerTexture.Width > windowWidth)
-            playerPosition.X = windowWidth - playerTexture.Width;
-
-        base.Update(gameTime);
+        playerVelocity.Y = jumpStrength;
+        isOnGround = false;
     }
+
+    // --- Gravitace
+    playerVelocity.Y += gravity;
+
+    // --- Předpovězená pozice hráče po aplikaci gravitace
+    Vector2 predictedPosition = playerPosition + new Vector2(0, playerVelocity.Y);
+
+    // --- Kontrola kolize s platformami
+    bool collisionWithPlatform = false;
+    foreach (var platformPos in platformPositions)
+    {
+        Rectangle platformRect = new Rectangle((int)platformPos.X, (int)platformPos.Y, 30, 30); // Platforma 30x30
+
+        // Vytvoření hranic hráče pro detekci všech stran
+        Rectangle futurePlayerBounds = new Rectangle((int)playerPosition.X, (int)predictedPosition.Y, 8, 8);
+
+        // --- Kolize s horní stranou platformy
+        if (futurePlayerBounds.Bottom > platformRect.Top && futurePlayerBounds.Top < platformRect.Top)
+        {
+            // Hráč narazil na horní stranu platformy (spadne dolů)
+            if (futurePlayerBounds.Right > platformRect.Left && futurePlayerBounds.Left < platformRect.Right)
+            {
+                playerPosition.Y = platformRect.Top - 8; // Umístíme hráče těsně nad platformu
+                playerVelocity.Y = 0;
+                isOnGround = true;
+                collisionWithPlatform = true;
+                break;
+            }
+        }
+
+        // --- Kolize s dolní stranou platformy
+        if (futurePlayerBounds.Top < platformRect.Bottom && futurePlayerBounds.Bottom > platformRect.Bottom)
+        {
+            // Hráč narazil na spodní stranu platformy (pokud se pohybuje vzhůru)
+            if (futurePlayerBounds.Right > platformRect.Left && futurePlayerBounds.Left < platformRect.Right)
+            {
+                playerPosition.Y = platformRect.Bottom; // Hráč je nad platformou
+                playerVelocity.Y = 0;
+            }
+        }
+
+        // --- Kolize s levým okrajem platformy
+        if (futurePlayerBounds.Right > platformRect.Left && futurePlayerBounds.Left < platformRect.Left)
+        {
+            // Hráč narazil na levý okraj platformy
+            if (futurePlayerBounds.Bottom > platformRect.Top && futurePlayerBounds.Top < platformRect.Bottom)
+            {
+                playerPosition.X = platformRect.Left - 8; // Umístíme hráče vlevo od platformy
+                playerVelocity.X = 0; // Zastavíme horizontální pohyb
+            }
+        }
+
+        // --- Kolize s pravým okrajem platformy
+        if (futurePlayerBounds.Left < platformRect.Right && futurePlayerBounds.Right > platformRect.Right)
+        {
+            // Hráč narazil na pravý okraj platformy
+            if (futurePlayerBounds.Bottom > platformRect.Top && futurePlayerBounds.Top < platformRect.Bottom)
+            {
+                playerPosition.X = platformRect.Right; // Umístíme hráče vpravo od platformy
+                playerVelocity.X = 0; // Zastavíme horizontální pohyb
+            }
+        }
+    }
+
+    // --- Pokud hráč není na platformě, aktualizuj jeho pozici (padá)
+    if (!collisionWithPlatform)
+    {
+        playerPosition += new Vector2(0, playerVelocity.Y);
+        isOnGround = false;
+    }
+
+    // --- Kontrola dopadu na spodní okraj okna
+    if (playerPosition.Y + playerTexture.Height >= windowHeight)
+    {
+        playerPosition.Y = windowHeight - playerTexture.Height;
+        playerVelocity.Y = 0;
+        isOnGround = true;
+    }
+
+    // --- Kontrola horního okraje okna
+    if (playerPosition.Y < 0)
+    {
+        playerPosition.Y = 0;
+        playerVelocity.Y = 0;
+    }
+
+    // --- Omezení pohybu vlevo a vpravo
+    if (playerPosition.X < 0)
+        playerPosition.X = 0;
+    if (playerPosition.X + playerTexture.Width > windowWidth)
+        playerPosition.X = windowWidth - playerTexture.Width;
+
+    base.Update(gameTime);
+}
 
     protected override void Draw(GameTime gameTime)
     {
