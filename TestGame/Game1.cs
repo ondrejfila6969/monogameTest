321c81afdbb0832fbd3d4d20a41efce1ba118499
Ondřej Fíla <ondrej.fila@student.spsmb.cz>
2025-04-23 10:36:57 +0200

    part2

--- TestGame/Game1.cs
+++ TestGame/Game1.cs
@@ -10,8 +10,10 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private Texture2D playerTexture;
     private Vector2 playerPosition;
+    private Vector2 playerVelocity; 
+    private const float gravity = 0.5f;  
+    private const float groundLevel = 400f; 
     
-
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -22,25 +24,32 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        playerTexture=Content.Load<Texture2D>("spritesheet");
-        playerPosition =new Vector2(200,200);
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        playerTexture = Content.Load<Texture2D>("spritesheet");
+        playerPosition = new Vector2(200, 200);  
+        playerVelocity = new Vector2(0, 0);
 
-        // TODO: use this.Content to load your game content here
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        // TODO: Add your update logic here
+        
+        playerVelocity.Y += gravity; 
+        
+        playerPosition += playerVelocity;
+        
+        if (playerPosition.Y > groundLevel)
+        {
+            playerPosition.Y = groundLevel;  
+            playerVelocity.Y = 0;  
+        }
 
         base.Update(gameTime);
     }
@@ -48,12 +57,13 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-        
+
         _spriteBatch.Begin();
         
         Rectangle rectangle = new Rectangle(0, 0, 8, 8);
-        _spriteBatch.Draw(playerTexture, playerPosition, rectangle, Color.White);
         
+        _spriteBatch.Draw(playerTexture, playerPosition, rectangle, Color.White);
+
         _spriteBatch.End();
 
         base.Draw(gameTime);
